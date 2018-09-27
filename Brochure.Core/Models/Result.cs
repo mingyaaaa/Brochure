83d72e5caac1e482fa8467c929b223f4d7b6032d
mingyaaaa <mingyaaaa@users.noreply.github.com>
2018-09-27 23:02:19 +0800

    添加Result类

---
+++ Brochure.Core/Models/Result.cs
@@ -0,0 +1,22 @@
+﻿namespace Brochure.Core.Models
+{
+    public class Result
+    {
+        public Result()
+        {
+        }
+
+        public Result(string msg)
+        {
+
+        }
+
+        public Result(int code, string msg)
+        {
+
+        }
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
