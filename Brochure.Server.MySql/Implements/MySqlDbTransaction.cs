4b97f0d0ce0d4d8a2085eecf8deedf82bfac4708
mingyaaaa <651017460@qq.com>
2018-11-05 23:29:57 +0800

    修改数据库操作方式

---
+++ Brochure.Server.MySql/Implements/MySqlDbTransaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Common;
+using Brochure.Core.Server;
+namespace Brochure.Server.MySql.Implements
+{
+    public class DbTransaction : IDbTransaction
+    {
+
+        public DbTransaction(System.Data.Common.DbTransaction dbTransaction)
+        {
+            Transaction = dbTransaction;
+        }
+
+        public System.Data.Common.DbTransaction Transaction { get; }
+
+        public void Commit()
+        {
+            Transaction.Commit();
+            Transaction.Connection.Close();
+        }
+
+        public void Rollback()
+        {
+            Transaction.Rollback();
+            Transaction.Connection.Close();
+        }
+    }
+}
