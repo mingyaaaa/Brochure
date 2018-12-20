cc609c1a7c3769c982e56aa90af0404f5d4bed19
mingyaaaa <mingyaaaa@users.noreply.github.com>
2018-12-20 21:56:52 +0800

    优化事件机制

---
+++ Brochure.Core.Server/core/EventManager.cs
@@ -0,0 +1,48 @@
+﻿using EventServer.Server;
+using System;
+using System.Collections.Generic;
+
+namespace Brochure.Core.Server
+{
+    public interface IEventManager
+    {
+        void RegistEvent(string eventName, string serviceKey, Action<object> action);
+        void RemoveEvent(string eventName, string serviceKey);
+        void Invoke(string eventName, string serviceKey, IRecord obj);
+
+    }
+    public class EventManager : IEventManager
+    {
+        private IDictionary<string, Action<object>> _eventCollection;
+        private string _publishServiceKey;
+        public EventManager(string publishServiceKey)
+        {
+            _eventCollection = new Dictionary<string, Action<Object>>();
+            _publishServiceKey = publishServiceKey;
+        }
+        public void RegistEvent(string eventName, string serviceKey, Action<object> action)
+        {
+            var rpc = new RpcClient<ISubscribeEventService.Client>(serviceKey);
+            if (rpc.Client.RegistEventType(eventName, _publishServiceKey))
+            {
+                var key = eventName + serviceKey;
+                _eventCollection.Add(key, action);
+            }
+        }
+        public void RemoveEvent(string eventName, string serviceKey)
+        {
+            var rpc = new RpcClient<ISubscribeEventService.Client>(serviceKey);
+            if (rpc.Client.RemoveEventType(eventName, _publishServiceKey))
+            {
+                var key = eventName + serviceKey;
+                _eventCollection.Remove(key);
+            }
+        }
+        public void Invoke(string eventName, string serviceKey, IRecord obj)
+        {
+            var action = _eventCollection[eventName + serviceKey];
+            action(obj);
+        }
+
+    }
+}
---
+++ Brochure.Core.Server/core/PublicshEventService.cs
@@ -0,0 +1,18 @@
+﻿using EventServer.Server;
+
+namespace Brochure.Core.Server
+{
+    public class PublicshEventService : IPublishEventService.Iface
+    {
+        private IEventManager _eventManager;
+        public PublicshEventService(IEventManager eventManager)
+        {
+            _eventManager = eventManager;
+        }
+        public void Invoke(string eventName, string serviceKey, string jsonParams)
+        {
+            var param = JsonUtil.ReadJson(jsonParams);
+            _eventManager.Invoke(eventName, serviceKey, param);
+        }
+    }
+}
