d40295166fdf0e0414e80ef57ff34aaf72ceb54c
yangming886 <651017460@qq.com>
2020-04-25 23:49:07 +0800

    添加RPC内存代理

--- Brochure.Core/Interceptor/InnerAssemblyInterceptor.cs
+++ Brochure.Core/Interceptor/InnerAssemblyInterceptor.cs
@@ -1,5 +1,5 @@
-using System.Linq;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using AspectCore.DynamicProxy;
 
@@ -7,19 +7,20 @@ namespace Brochure.Core.Interceptor
 {
     public class InnerAssemblyInterceptor : AbstractInterceptor
     {
-        private readonly Type impType;
+        private readonly object impObj;
 
-        public InnerAssemblyInterceptor(Type type)
+        public InnerAssemblyInterceptor (object obj)
         {
-            this.impType = type;
+            this.impObj = obj;
         }
-        public override Task Invoke(AspectContext context, AspectDelegate next)
+        public override Task Invoke (AspectContext context, AspectDelegate next)
         {
+            var impType = impObj.GetType ();
             var proxyMethod = context.ProxyMethod;
-            var impMethod = Array.Find(impType.GetMethods(), t => t.Name == proxyMethod.Name);
+            var impMethod = Array.Find (impType.GetMethods (), t => t.Name == proxyMethod.Name);
             if (impMethod == null)
-                throw new Exception($"{impType}实现类中没有{proxyMethod.Name}方法");
-            context.ReturnValue = impMethod.Invoke(context.Proxy, context.Parameters);
+                throw new Exception ($"{impType}实现类中没有{proxyMethod.Name}方法");
+            context.ReturnValue = impMethod.Invoke (impObj, context.Parameters);
             return Task.CompletedTask;
         }
     }
--- Brochure.Core/RPC/Rpc.cs
+++ Brochure.Core/RPC/Rpc.cs
@@ -7,22 +7,22 @@ using AspectCore.Configuration;
 using AspectCore.DynamicProxy;
 using Brochure.Abstract;
 using Brochure.Core.Interceptor;
-using Polly;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
 
 namespace Brochure.Core.RPC
 {
     public class Rpc<T> : IRpcProxy<T> where T : class
     {
-        public Rpc(IRpcProxyFactory factory, params object[] args)
+        public Rpc (IRpcProxyFactory factory, params object[] args)
         {
-            Ins = factory.CreateRpcProxy<T>(args);
+            Ins = factory.CreateRpcProxy<T> (args);
         }
         public T Ins { get; }
     }
     public interface IRpcProxyFactory
     {
-        T CreateRpcProxy<T>(params object[] args) where T : class;
+        T CreateRpcProxy<T> (params object[] args) where T : class;
     }
 
     /// <summary>
@@ -32,35 +32,43 @@ namespace Brochure.Core.RPC
     {
         private readonly PollyOption option;
 
-        public RpcPollyProxyFactory(PollyOption option)
+        public RpcPollyProxyFactory (PollyOption option)
         {
             this.option = option;
         }
-        public T CreateRpcProxy<T>(params object[] args) where T : class
+        public T CreateRpcProxy<T> (params object[] args) where T : class
         {
             //创建代理类                                                                                                             
-            var builder = new ProxyGeneratorBuilder();
-            builder.Configure(t => t.Interceptors.AddTyped<PollyInterceptor>(new object[] { option }));
-            var type = typeof(T);
-            var proxyCreate = builder.Build();
-            return proxyCreate.CreateClassProxy<T>(args);
+            var builder = new ProxyGeneratorBuilder ();
+            builder.Configure (t => t.Interceptors.AddTyped<PollyInterceptor> (new object[] { option }));
+            var type = typeof (T);
+            var proxyCreate = builder.Build ();
+            return proxyCreate.CreateClassProxy<T> (args);
         }
     }
 
     public class RpcMemoryProxyFactory : IRpcProxyFactory
     {
-        private readonly Type targetType;
+        private readonly object targetObj;
 
-        public RpcMemoryProxyFactory(Type targetType)
+        public RpcMemoryProxyFactory (object targetObj)
         {
-            this.targetType = targetType;
+            this.targetObj = targetObj;
         }
-        public T CreateRpcProxy<T>(params object[] args) where T : class
+        public RpcMemoryProxyFactory (Type type)
         {
-            var builder = new ProxyGeneratorBuilder();
-            builder.Configure(t => t.Interceptors.AddTyped<InnerAssemblyInterceptor>(new object[] { targetType }));
-            var proxyCreate = builder.Build();
-            return proxyCreate.CreateClassProxy<T>(args);
+            this.targetObj = Activator.CreateInstance (type);
+        }
+        public RpcMemoryProxyFactory (Func<object> func)
+        {
+            targetObj = func.Invoke ();
+        }
+        public T CreateRpcProxy<T> (params object[] args) where T : class
+        {
+            var builder = new ProxyGeneratorBuilder ();
+            builder.Configure (t => t.Interceptors.AddTyped<InnerAssemblyInterceptor> (new object[] { targetObj }));
+            var proxyCreate = builder.Build ();
+            return proxyCreate.CreateClassProxy<T> (args);
         }
     }
 }
