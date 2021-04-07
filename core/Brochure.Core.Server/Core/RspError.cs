e59871db408accb7e141d6787731e493ee9f727e
杨明 <mingy651017460@outlook.com>
2021-04-07 22:53:15 +0800

    添加返回错误的静态类型

---
+++ core/Brochure.Core.Server/Core/RspError.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brochure.Core.Server.Core
+{
+    /// <summary>
+    /// The rsp error.
+    /// </summary>
+    public class RspError
+    {
+        /// <summary>
+        /// Jsons the error.
+        /// </summary>
+        /// <param name="code">The code.</param>
+        /// <param name="jsonObject">The json object.</param>
+        /// <returns>A JsonResult.</returns>
+        public JsonResult JsonError(object jsonObject, int code = 500)
+        {
+            var r = new JsonResult(jsonObject);
+            r.StatusCode = code;
+            return r;
+        }
+
+        /// <summary>
+        /// Contents the error.
+        /// </summary>
+        /// <param name="code">The code.</param>
+        /// <param name="content">The content.</param>
+        /// <returns>A ContentResult.</returns>
+        public ContentResult ContentError(string content, int code = 500)
+        {
+            var r = new ContentResult();
+            r.Content = content;
+            r.StatusCode = code;
+            return r;
+        }
+    }
+}
